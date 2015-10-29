a912d91a6890f2d90d38e90615d8d9847889cd59
t0s <t00s@ya.ru>
2015-10-29 17:42:47 +0300

    gulp-babel instead gulp-react

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var minifyCSS = require('gulp-minify-css');
 var react = require('gulp-react');
+var babel = require('gulp-babel');
 
 gulp.task('useref', function(){
 	var assets = useref.assets();
@@ -17,8 +18,14 @@ gulp.task('useref', function(){
 		.pipe(gulp.dest(''))
 });
 
-gulp.task('react', function () {
-    return gulp.src('dev/js/*.jsx')
+/*gulp.task('react', function () {
+    return gulp.src('dev/js/!*.jsx')
         .pipe(react({harmony:true}))
+        .pipe(gulp.dest('dev/js/'));
+});*/
+
+gulp.task('babel', function(){
+    return gulp.src('dev/js/*.jsx')
+        .pipe(babel())
         .pipe(gulp.dest('dev/js/'));
 });
