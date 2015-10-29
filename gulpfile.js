c6295ffae82f3ecd9210d6953da610d536288c82
t0s <t00s@ya.ru>
2015-10-29 17:16:14 +0300

    trying to execure jsx translate

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var minifyCSS = require('gulp-minify-css');
+var react = require('gulp-react');
 
 gulp.task('useref', function(){
 	var assets = useref.assets();
 
-	return gulp.src('*.html')
+	return gulp.src('dev/*.html')
 		.pipe(assets)
+		.pipe(gulpIf('*.css', minifyCSS())) // Uglifies CSS files
+		.pipe(gulpIf('*.js', uglify()))	// Uglifies JS files
 		.pipe(assets.restore())
 		.pipe(useref())
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest(''))
+});
+
+gulp.task('react', function () {
+    return gulp.src('dev/js/*.jsx')
+        .pipe(react({harmony:true}))
+        .pipe(gulp.dest('dev/js/'));
 });
